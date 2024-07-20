6d19882b3e74f33df4d05272720496c3dda24b72
SoyaChunkz <sameermaroof26@gmail.com>
2024-07-20 22:10:43 +0530

    initial commit

---
+++ Readme.md
@@ -0,0 +1,65 @@
+# Sudoku Solver
+
+A simple Sudoku Solver application written in Java using Swing for the user interface. This application can generate a Sudoku puzzle, allow user input, solve the puzzle, and validate the solution.
+
+## Features
+
+- **Generate Random Board**: Create a new Sudoku puzzle with a random configuration.
+- **Solve**: Automatically solve the current Sudoku puzzle.
+- **Check Solution**: Verify if the current solution is correct.
+
+## Screenshot
+
+**Click on the Generate Board button to get a random board**
+![Sudoku Solver Screenshot](/screenshots/generate_sudoku.png)
+
+**Click on the Solve button to get the solved board**
+![Sudoku Solver Screenshot](/screenshots/solve_sudoku.png)
+
+**Click on the Check button to check your solution**
+![Sudoku Solver Screenshot](/screenshots/check_solution.png)
+
+## Getting Started
+
+Follow these steps to get the project up and running:
+
+### Prerequisites
+
+- Java Development Kit (JDK) 8 or higher
+
+### Installation
+
+1. **Clone the Repository**
+
+   ```sh
+   git clone https://github.com/yourusername/SudokuSolver.git
+    ```
+
+2. **Navigate to the Project Directory**
+   ```sh
+    cd SudokuSolver
+    ```
+
+3. **Compile the Code**
+   ```sh
+    javac SudokuSolver.java
+    ```
+
+4. **Run the Application**
+   ```sh
+    java SudokuSolver
+    ```
+
+### Usage
+
+1. **Generate a Random Board: Click on the "Generate Random Board" button to create a new Sudoku puzzle.**
+2. **Solve the Puzzle: Click on the "Solve" button to find a solution for the current puzzle.**
+3. **Check Your Solution: Click on the "Check Solution" button to verify if the numbers you entered are correct.**
+
+### Code Overview
+
+**SudokuSolver.java: Contains the main application code, including Sudoku generation, solving algorithms, and GUI.**
+
+### Contact
+
+**For questions or feedback, please contact [sameermaroof26@gmail.com] or open an issue in the repository.**
---
+++ SudokuUI.java
@@ -0,0 +1,210 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+public class SudokuUI {
+
+    private static final int SIZE = 9;
+    private static final int SUBGRID_SIZE = 3;
+    private static final Random random = new Random();
+    private static char[][] board = new char[SIZE][SIZE];
+    
+    private static JTextField[][] textFields = new JTextField[SIZE][SIZE];
+
+    private static boolean isSafe(int row, int col, char[][] board, int number) {
+        char numChar = (char) (number + '0');
+        for (int i = 0; i < SIZE; i++) {
+            if (board[row][i] == numChar || board[i][col] == numChar) {
+                return false;
+            }
+        }
+        int startRow = (row / SUBGRID_SIZE) * SUBGRID_SIZE;
+        int startCol = (col / SUBGRID_SIZE) * SUBGRID_SIZE;
+        for (int i = startRow; i < startRow + SUBGRID_SIZE; i++) {
+            for (int j = startCol; j < startCol + SUBGRID_SIZE; j++) {
+                if (board[i][j] == numChar) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static boolean solveSudoku(char[][] board, int row, int col) {
+        if (row == SIZE) {
+            return true;
+        }
+        int nextRow = (col == SIZE - 1) ? row + 1 : row;
+        int nextCol = (col == SIZE - 1) ? 0 : col + 1;
+        if (board[row][col] != '.') {
+            return solveSudoku(board, nextRow, nextCol);
+        }
+        for (int num = 1; num <= 9; num++) {
+            if (isSafe(row, col, board, num)) {
+                board[row][col] = (char) (num + '0');
+                if (solveSudoku(board, nextRow, nextCol)) {
+                    return true;
+                }
+                board[row][col] = '.'; // Backtrack
+            }
+        }
+        return false;
+    }
+
+    private static void generateCompleteBoard(char[][] board) {
+        solveSudoku(board, 0, 0);
+    }
+
+    public static void generateRandomBoard() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                board[i][j] = '.';
+            }
+        }
+        generateCompleteBoard(board);
+        int cellsToRemove = 40;
+        while (cellsToRemove > 0) {
+            int row = random.nextInt(SIZE);
+            int col = random.nextInt(SIZE);
+            if (board[row][col] != '.') {
+                board[row][col] = '.';
+                cellsToRemove--;
+            }
+        }
+        updateUI();
+    }
+
+    private static void updateUI() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                textFields[i][j].setText(board[i][j] == '.' ? "" : String.valueOf(board[i][j]));
+            }
+        }
+    }
+
+    private static boolean isValidBoard(char[][] board) {
+        for (int i = 0; i < SIZE; i++) {
+            boolean[] rowCheck = new boolean[SIZE];
+            boolean[] colCheck = new boolean[SIZE];
+
+            for (int j = 0; j < SIZE; j++) {
+                if (board[i][j] != '.' && rowCheck[board[i][j] - '1']) {
+                    return false; // Duplicate in row
+                }
+                rowCheck[board[i][j] - '1'] = true;
+
+                if (board[j][i] != '.' && colCheck[board[j][i] - '1']) {
+                    return false; // Duplicate in column
+                }
+                colCheck[board[j][i] - '1'] = true;
+            }
+        }
+
+        for (int blockRow = 0; blockRow < SUBGRID_SIZE; blockRow++) {
+            for (int blockCol = 0; blockCol < SUBGRID_SIZE; blockCol++) {
+                boolean[] gridCheck = new boolean[SIZE];
+                for (int i = 0; i < SUBGRID_SIZE; i++) {
+                    for (int j = 0; j < SUBGRID_SIZE; j++) {
+                        char num = board[blockRow * SUBGRID_SIZE + i][blockCol * SUBGRID_SIZE + j];
+                        if (num != '.' && gridCheck[num - '1']) {
+                            return false; // Duplicate in 3x3 grid
+                        }
+                        gridCheck[num - '1'] = true;
+                    }
+                }
+            }
+        }
+
+        return true; // All checks passed
+    }
+
+    private static void solveBoard() {
+        char[][] boardArray = new char[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                boardArray[i][j] = textFields[i][j].getText().isEmpty() ? '.' : textFields[i][j].getText().charAt(0);
+            }
+        }
+        if (solveSudoku(boardArray, 0, 0)) {
+            board = boardArray;
+            updateUI();
+        } else {
+            JOptionPane.showMessageDialog(null, "No solution");
+        }
+    }
+
+    private static void checkSolution() {
+        char[][] boardArray = new char[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                boardArray[i][j] = textFields[i][j].getText().isEmpty() ? '.' : textFields[i][j].getText().charAt(0);
+            }
+        }
+        if (isValidBoard(boardArray)) {
+            JOptionPane.showMessageDialog(null, "Yayyy! The solution is correct");
+        } else {
+            JOptionPane.showMessageDialog(null, "Oops! The solution is incorrect, Please try again.");
+        }
+    }
+
+    public static void createAndShowGUI() {
+        JFrame frame = new JFrame("Sudoku Solver");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+        JPanel gridPanel = new JPanel(new GridLayout(SIZE, SIZE));
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                textFields[i][j] = new JTextField(2);
+                textFields[i][j].setHorizontalAlignment(JTextField.CENTER);
+                gridPanel.add(textFields[i][j]);
+            }
+        }
+
+        JButton generateButton = new JButton("Generate Random Board");
+        generateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                generateRandomBoard();
+            }
+        });
+
+        JButton solveButton = new JButton("Solve");
+        solveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                solveBoard();
+            }
+        });
+
+        JButton checkButton = new JButton("Check Solution");
+        checkButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkSolution();
+            }
+        });
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(generateButton);
+        buttonPanel.add(solveButton);
+        buttonPanel.add(checkButton);
+
+        frame.add(gridPanel, BorderLayout.CENTER);
+        frame.add(buttonPanel, BorderLayout.SOUTH);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
